b156edefe241c8c1540d1e5d3b490ac1170f46da
popisgod <ronglozman2006@gmail.com>
2023-12-23 11:29:39 +0200

    add small fix

--- BackdoorCTF/Forenscript/solution.py
+++ BackdoorCTF/Forenscript/solution.py
@@ -1,15 +1,10 @@
-binary_data = b''
+result_data = b''
 with open('public\\a.bin', 'rb') as file:
     while True: 
-        first_part = file.read(2)
-        second_part = file.read(2)
-
-        if not second_part: 
+        binary_data = file.read(4)
+        if not binary_data:
             break 
-        
-        binary_data += second_part[::-1]
-        binary_data += first_part[::-1]
-
+        result_data += binary_data[::-1]
         
 with open('output.png', 'wb') as output_file: 
-    output_file.write(binary_data)
+    output_file.write(result_data)
