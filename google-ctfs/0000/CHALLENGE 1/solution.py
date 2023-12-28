f597010872beff197906602143516fcc43166627
popisgod <ronglozman2006@gmail.com>
2023-12-28 12:13:10 +0200

    add pwnable coin1 solution

--- google-ctfs/0000/CHALLENGE 1/solution.py
+++ google-ctfs/0000/CHALLENGE 1/solution.py
@@ -1,15 +1,9 @@
-with open('encrypted_message.txt', 'r') as file:
-    msg = file.readline().lower()
+import ctypes
+random_value = 0x18ec8348
+expected_result = 0xdeadbeef
 
-shift_value = [2, 0, 4, 17 ]
-decoded_msg = ''
-
-index = 0 
-for ch in msg: 
-    if ch.isalpha(): 
-        decoded_msg += chr((((ord(ch) - ord('a')) - shift_value[index % 4]) % 26) + ord('a')) 
-        index += 1
-    else: 
-        decoded_msg += ch 
-        
-print(decoded_msg)
+key = expected_result ^ random_value
+print(key)
+signed_value = ctypes.c_int32(key).value
+print(signed_value)
+assert (key ^ random_value) == 0xdeadbeef
